b82e8597ab1ad6ac585cac3b98591c9978960bf4
Erwin Lenting <erwinlenting@outlook.com>
2021-05-30 00:20:50 +0200

    Second part of settings

--- EnergyPortal/frontend/src/components/settings/AppSettings.vue
+++ EnergyPortal/frontend/src/components/settings/AppSettings.vue
@@ -7,7 +7,6 @@
                     <v-card-text>
                         <v-form
                             ref="generalSettings"
-                            v-model="valid"
                             lazy-validation
                         >
                             <v-switch
@@ -40,7 +39,92 @@
                         <v-btn
                             :disabled="!generalSettingsChanged"
                             color="success"
-                            @click="saveSettings"
+                            @click="saveGeneralSettings"
+                            text
+                        >
+                            Opslaan
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-card elevation="3">
+                    <v-card-title>Tarieven</v-card-title>
+                    <v-card-text>
+                        <v-form
+                            ref="generalSettings"
+                            lazy-validation
+                        >
+                            <v-text-field
+                                v-model="prices.highUsagePricePerKwh"
+                                label="Opname normaaltarief"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.lowUsagePricePerKwh"
+                                label="Opname daltarief"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.highRedeliveryPricePerKwh"
+                                label="Teruglevering normaaltarief"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.lowRedeliveryPricePerKwh"
+                                label="Teruglevering daltarief"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.gasPrice"
+                                label="Gastarief"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.electricityDeliveryPricePerMonth"
+                                label="Vaste kosten electriciteit / maand"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="prices.gasDeliveryPricePerMonth"
+                                label="Vaste kosten gas / maand"
+                                prefix="€"
+                                type="number"
+                                step="0.0000000001"
+                            ></v-text-field>
+                            
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                            :disabled="!pricesChanged"
+                            color="error"
+                            class="mr-4"
+                            @click="resetPrices"
+                            text
+                        >
+                            Reset
+                        </v-btn>
+                        <v-btn
+                            :disabled="!pricesChanged"
+                            color="success"
+                            @click="savePrices"
                             text
                         >
                             Opslaan
@@ -69,39 +153,39 @@ export default {
             showDayName: false,
             selectedTimeZone: null,
         },
-        timeZoneOptions: [],
-        highUsagePricePerKwh: 0.0 ,
-        lowUsagePricePerKwh: 0.0 ,
-        highRedeliveryPricePerKwh: 0.0 ,
-        lowRedeliveryPricePerKwh: 0.0 ,
-        gasPrice: 0.0 ,
-        electricityDeliveryPricePerMonth: 0.0 ,
-        gasDeliveryPricePerMonth: 0.0 ,
-        
-        valid: true,
-        name: '',
-        nameRules: [
-            v => !!v || 'Name is required',
-            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        ],
-        email: '',
-        emailRules: [
-            v => !!v || 'E-mail is required',
-            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        ],
-        select: null,
-        items: [
-            'Item 1',
-            'Item 2',
-            'Item 3',
-            'Item 4',
-        ],
-        checkbox: false,
+        prices: {
+            highUsagePricePerKwh: 0.0,
+            lowUsagePricePerKwh: 0.0,
+            highRedeliveryPricePerKwh: 0.0,
+            lowRedeliveryPricePerKwh: 0.0,
+            gasPrice: 0.0,
+            electricityDeliveryPricePerMonth: 0.0,
+            gasDeliveryPricePerMonth: 0.0,
+        },
+        originalPrices: {
+            highUsagePricePerKwh: 0.0,
+            lowUsagePricePerKwh: 0.0,
+            highRedeliveryPricePerKwh: 0.0,
+            lowRedeliveryPricePerKwh: 0.0,
+            gasPrice: 0.0,
+            electricityDeliveryPricePerMonth: 0.0,
+            gasDeliveryPricePerMonth: 0.0,
+        },
+        timeZoneOptions: []
     }),
     computed: {
         generalSettingsChanged() {
             for (const [key, value] of Object.entries(this.originalSettings)) {
                 if (this.generalSettings[key] !== value) {
+                    return true;
+                }
+            }
+            
+            return false;
+        },
+        pricesChanged() {
+            for (const [key, value] of Object.entries(this.originalPrices)) {
+                if (this.prices[key] !== value) {
                     return true;
                 }
             }
@@ -133,13 +217,14 @@ export default {
                 this.originalSettings.selectedTimeZone = response.data.timeZoneId;
                 this.resetGeneralSettings();
                 
-                this.highUsagePricePerKwh = response.data.highUsagePricePerKwh;
-                this.lowUsagePricePerKwh = response.data.lowUsagePricePerKwh;
-                this.highRedeliveryPricePerKwh = response.data.highRedeliveryPricePerKwh;
-                this.lowRedeliveryPricePerKwh = response.data.lowRedeliveryPricePerKwh;
-                this.gasPrice = response.data.gasPrice;
-                this.electricityDeliveryPricePerMonth = response.data.electricityDeliveryPricePerMonth;
-                this.gasDeliveryPricePerMonth = response.data.gasDeliveryPricePerMonth;
+                this.originalPrices.highUsagePricePerKwh = response.data.highUsagePricePerKwh;
+                this.originalPrices.lowUsagePricePerKwh = response.data.lowUsagePricePerKwh;
+                this.originalPrices.highRedeliveryPricePerKwh = response.data.highRedeliveryPricePerKwh;
+                this.originalPrices.lowRedeliveryPricePerKwh = response.data.lowRedeliveryPricePerKwh;
+                this.originalPrices.gasPrice = response.data.gasPrice;
+                this.originalPrices.electricityDeliveryPricePerMonth = response.data.electricityDeliveryPricePerMonth;
+                this.originalPrices.gasDeliveryPricePerMonth = response.data.gasDeliveryPricePerMonth;
+                this.resetPrices();
             } catch (e) {
                 console.error(e);
             } finally {
@@ -157,7 +242,7 @@ export default {
 
             this.loading = false;
         },
-        async saveSettings() {
+        async saveGeneralSettings() {
             if (!this.$refs.generalSettings.validate()) {
                 return;
             }
@@ -169,13 +254,49 @@ export default {
                     solarSystem: this.generalSettings.solarSystem,
                     showDayName: this.generalSettings.showDayName,
                     timeZoneId: this.generalSettings.selectedTimeZone,
-                    highUsagePricePerKwh: this.highUsagePricePerKwh,
-                    lowUsagePricePerKwh: this.lowUsagePricePerKwh,
-                    highRedeliveryPricePerKwh: this.highRedeliveryPricePerKwh,
-                    lowRedeliveryPricePerKwh: this.lowRedeliveryPricePerKwh,
-                    gasPrice: this.gasPrice,
-                    electricityDeliveryPricePerMonth: this.electricityDeliveryPricePerMonth,
-                    gasDeliveryPricePerMonth: this.gasDeliveryPricePerMonth,
+                    highUsagePricePerKwh: this.originalPrices.highUsagePricePerKwh,
+                    lowUsagePricePerKwh: this.originalPrices.lowUsagePricePerKwh,
+                    highRedeliveryPricePerKwh: this.originalPrices.highRedeliveryPricePerKwh,
+                    lowRedeliveryPricePerKwh: this.originalPrices.lowRedeliveryPricePerKwh,
+                    gasPrice: this.originalPrices.gasPrice,
+                    electricityDeliveryPricePerMonth: this.originalPrices.electricityDeliveryPricePerMonth,
+                    gasDeliveryPricePerMonth: this.originalPrices.gasDeliveryPricePerMonth,
+                }
+
+                await Axios.put('/webapi/v3/settings', settings);
+                await this.fetchSettings();
+                // this.makeToast('Successfully saved settings!', 'success');
+            } catch (e) {
+                console.error(e);
+                // this.makeToast('Failed to save settings.', 'danger');
+            } finally {
+                this.saving = false;
+            }
+        },
+        resetPrices() {
+            for (const [key, value] of Object.entries(this.originalPrices)) {
+                this.prices[key] = value;
+            }
+        },
+        async savePrices() {
+            if (!this.$refs.generalSettings.validate()) {
+                return;
+            }
+            
+            try {
+                this.saving = true;
+
+                let settings = {
+                    solarSystem: this.originalPrices.solarSystem,
+                    showDayName: this.originalPrices.showDayName,
+                    timeZoneId: this.originalPrices.selectedTimeZone,
+                    highUsagePricePerKwh: this.prices.highUsagePricePerKwh,
+                    lowUsagePricePerKwh: this.prices.lowUsagePricePerKwh,
+                    highRedeliveryPricePerKwh: this.prices.highRedeliveryPricePerKwh,
+                    lowRedeliveryPricePerKwh: this.prices.lowRedeliveryPricePerKwh,
+                    gasPrice: this.prices.gasPrice,
+                    electricityDeliveryPricePerMonth: this.prices.electricityDeliveryPricePerMonth,
+                    gasDeliveryPricePerMonth: this.prices.gasDeliveryPricePerMonth,
                 }
 
                 await Axios.put('/webapi/v3/settings', settings);
